9beed94efd00fed2be1ae6429b88212733a45641
leosalvi03 <leonardosalviato03@gmail.com>
2024-05-15 20:43:18 +0200

    Ridenominazione directory con spazio alla fine

---
+++ src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////
+// Riccardo Stefani 2068225
+// Leonardo Salviato 2068222
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
